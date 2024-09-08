66f40fa19fe3e041b9e7b456dbba46bf4d3ce604
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-09-08 15:41:32 +0900

    Update main_screen.dart

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,11 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+              child: SvgPicture.asset(
+                "assets/images/SignUp.svg",
+                fit: BoxFit.cover,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    "Stay updated!",
+                    style: TextStyle(
+                      color: Color.fromRGBO(32, 35, 58, 1),
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Join 60,000+ product managers receiving monthly updates on:",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color.fromRGBO(61, 61, 74, 1),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
